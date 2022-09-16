a5bb958b40182b6cc6da770d164ee48982560445
sahil bansal <sahilb20@iitk.ac.in>
2022-09-16 19:28:40 +0530

    added sdcm and gd

--- assn1/submit.py
+++ assn1/submit.py
@@ -5,6 +5,195 @@ from scipy.linalg import khatri_rao
 import random as rnd
 import time as tm
 
+
+
+
+
+def stepLengthGenerator( mode, eta ):
+	if mode == "constant":
+		return lambda t: eta
+	elif mode == "linear":
+		return lambda t: eta/(t+1)
+	elif mode == "quadratic":
+		return lambda t: eta/np.sqrt(t+1)
+
+class SVM:
+    def __init__(self, X, y, C):
+        self.X = X.copy()
+        self.y = y.copy()
+        self.C = C
+        self.n = X.shape[0]
+        self.d = X.shape[1]
+        self.__intialize_params__()
+
+    def __intialize_params__(self):
+        X = self.X
+        y = self.y
+
+		# param intiliaziation for SDCM
+		
+        initDual = self.C * np.ones((self.n))
+        self.normSq = np.square( np.linalg.norm( X, axis = 1 ) ) + 1
+        self.w_SDCM = X.T.dot( np.multiply( initDual, y ) )
+        self.alpha = initDual
+
+		# param init for GD
+        self.theta = np.zeros((self.d))
+    
+    def predict(self, X):
+
+        y_new = X.dot(self.w_SDCM) 
+        return y_new
+
+    def doCoordOptCSVMDual(self, i ):
+        x = self.X[i,:]
+        y = self.y
+        w_SDCM = self.w_SDCM
+        C = self.C
+
+        # Find the unconstrained new optimal value of alpha_i
+        # It takes only O(d) time to do so because of our clever book keeping
+        newAlphai = (1 - y[i] * x.dot(w_SDCM) ) / self.normSq[i]
+        
+        # Make sure that the constraints are satisfied. This takes only O(1) time
+        if newAlphai > C:
+            newAlphai = C
+        if newAlphai < 0:
+            newAlphai = 0
+
+        # Update the primal model vector and bias values to ensure bookkeeping is proper
+        # Doing these bookkeeping updates also takes only O(d) time
+        self.w_SDCM = w_SDCM + (newAlphai - self.alpha[i]) * y[i] * x
+        
+        return newAlphai
+
+    def getCSVMPrimalDualObjVals( self ):
+        w_SDCM = self.w_SDCM
+        X = self.X
+        y = self.y
+        C = self.C
+
+        hingeLoss = np.maximum( 1 - np.multiply( (X.dot( w_SDCM )), y ), 0 )
+        objPrimal = 0.5 * w_SDCM.dot( w_SDCM ) + C * np.sum(hingeLoss)
+        # Recall that b is supposed to be treated as the last coordinate of w
+        objDual = np.sum( self.alpha ) - 0.5 * np.square( np.linalg.norm( w_SDCM ) )
+        
+        return np.array( [objPrimal, objDual] )
+
+    def getRandpermCoord( self, state ):
+        idx = state[0]
+        perm = state[1]
+        d = len( perm )
+        if idx >= d - 1 or idx < 0:
+            idx = 0
+            perm = np.random.permutation( d )
+        else:
+            idx += 1
+        state = (idx, perm)
+        curr = perm[idx]
+        return (curr, state)
+    
+    def doSDCM(self, horizon = 10 ):
+        objValSeries = []
+        timeSeries = []
+        totTime = 0
+        d = self.d
+        state = (-1,np.random.permutation( d ))
+        # Initialize model as well as some bookkeeping variables
+        
+        for it in range( horizon ):
+            # Start a stopwatch to calculate how much time we are spending
+            tic = tm.perf_counter()
+            
+            # Get the next coordinate to update and update that coordinate
+            (i, state) = self.getRandpermCoord( state )
+            (self.alpha)[i] = self.doCoordOptCSVMDual(  i)
+
+            toc = tm.perf_counter()
+            totTime = totTime + (toc - tic)
+            # print('\r Accuracy:', self.eval(self.X, self.y), end='')
+            objValSeries.append( self.getCSVMPrimalDualObjVals() )
+            timeSeries.append( totTime )
+            
+        return (objValSeries, timeSeries)
+
+    def getCSVMGrad(self, theta ):
+        y = self.y
+        C = self.C
+        X = self.X
+
+        w = theta
+        discriminant = np.multiply( (X.dot( w )), y )
+        g = np.zeros( (y.size,) )
+        g[discriminant < 1] = -1
+        # delb = C * g.dot( y )
+        delw = w + C * (X.T * g).dot( y )
+        return delw
+
+    def getCSVMObjVal(self,  theta ):
+        C = self.C
+        X = self.X
+        y = self.y
+
+        w = theta
+        hingeLoss = np.maximum( 1 - np.multiply( (X.dot( w )), y ), 0 )
+        return 0.5 * w.dot( w ) + C * np.sum( hingeLoss )
+    
+    def clean_up(self, cumulative, doModelAveraging, it ):
+        final = 0
+        if doModelAveraging:
+            final = cumulative / (it + 1)
+        else:
+            final = cumulative
+        
+        self.theta = final
+        self.w_SDCM = final
+	
+    def doGD( self, stepFunc , doModelAveraging, spacing, timeout):
+        objValSeries = []
+        timeSeries = []
+        totTime = 0
+        theta = self.theta
+        cumulative = self.theta
+        it = 1
+        
+        while True:
+            # Start a stopwatch to calculate how much time we are spending
+            tic = tm.perf_counter()
+            delta = self.getCSVMGrad( theta )
+            theta = theta - stepFunc( it + 1 ) * delta
+            # If we are going to do model averaging, just keep adding the models
+            if doModelAveraging:
+                cumulative = cumulative + theta
+            else:
+                cumulative = theta
+            # All calculations done -- stop the stopwatch
+            toc = tm.perf_counter()
+            totTime = totTime + (toc - tic)
+            # If model averaging is being done, need to calculate current objective value a bit differently
+            if doModelAveraging:
+                objValSeries.append( self.getCSVMObjVal( cumulative/(it+2) ) )
+            else:
+                objValSeries.append( self.getCSVMObjVal( cumulative ) )
+
+            timeSeries.append( totTime )
+            
+            if it%spacing ==0 and totTime > timeout:
+                self.clean_up(cumulative, doModelAveraging, it)
+                return (objValSeries, timeSeries)
+            it+=1
+
+
+    def eval(self, X_t, y_t):
+        y_t_pred = self.predict( X_t )
+        y_t_pred = np.where( y_t_pred > 0, 1, -1 )
+        acc = np.average( y_t == y_t_pred )
+        return acc
+
+
+
+
+
 # SUBMIT YOUR CODE AS A SINGLE PYTHON (.PY) FILE INSIDE A ZIP ARCHIVE
 # THE NAME OF THE PYTHON FILE MUST BE submit.py
 # DO NOT INCLUDE PACKAGES LIKE SKLEARN, SCIPY, KERAS ETC IN YOUR CODE
@@ -58,7 +247,10 @@ def get_features( X ):
 	# let n = number of training examples.
 
 	# X is n * 8 matrix
-	X_inter = np.insert(X, 0, 1,axis = 1)			# X_inter is n*9 mat
+	D = np.flip(2*X-1,axis = 1)
+	X_inter = np.cumprod(D, axis = 1)
+
+	X_inter = np.insert(X_inter, 0, 1,axis = 1)			# X_inter is n*9 mat
 
 	X_inter = X_inter.T								# X_inter is 9*n mat
 
@@ -72,9 +264,6 @@ def get_features( X ):
 # Non Editable Region Starting #
 ################################
 def solver( X, y, timeout, spacing ):
-	(n, d) = X.shape
-	t = 0
-	totTime = 0
 	
 	# W is the model vector and will get returned once timeout happens
 	# B is the bias term that will get returned once timeout happens
@@ -86,7 +275,11 @@ def solver( X, y, timeout, spacing ):
 	# returned when timeout happens. Take care to update W, B whenever you update your W_extended
 	# variable otherwise you will get wrong results.
 	# Also note that the dimensionality of W may be larger or smaller than 9
-	
+
+
+	(n, d) = X.shape
+	t = 0
+	totTime = 0
 	W = []
 	B = 0
 	tic = tm.perf_counter()
@@ -100,7 +293,8 @@ def solver( X, y, timeout, spacing ):
 ################################
 # Non Editable Region Starting #
 ################################
-	while True:
+
+    while True:
 		t = t + 1
 		if t % spacing == 0:
 			toc = tm.perf_counter()
